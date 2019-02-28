763b296b76159c22241ff07044e9ff7b06cf9a10
Deric Walintukan <deric.walintukan@gmail.com>
2019-02-28 16:37:58 +0700

    Use rlp lib for encoding script

--- script/encode.js
+++ script/encode.js
@@ -1,28 +1,40 @@
 require('dotenv').config()
 const { isUndefined } = require('lodash')
 const Web3 = require('web3')
-const Transaction = require('nakajs-tx')
+const RLP = require('rlp')
 
 const getEnv = require('./get-env')
 
 const web3 = new Web3()
 const envParams = getEnv()
 const txParams = {
-  chainId: envParams.chainId,
-  to: envParams.to,
   nonce: web3.utils.toHex(!isUndefined(envParams.nonce) ? envParams.nonce : 0),
-  gasLimit: web3.utils.numberToHex(envParams.gasLimit || 21000),
   gasPrice: !isUndefined(envParams.gasPrice) ? web3.utils.numberToHex(envParams.gasPrice) : '0xB165100C4',
+  gasLimit: web3.utils.numberToHex(envParams.gasLimit || 21000),
+  to: envParams.to,
   value: web3.utils.numberToHex(envParams.value || '0'),
   data: envParams.data,
   token: envParams.token,
   exchanger: envParams.exchanger,
   exchangeRate: envParams.exchangeRate,
+  v: envParams.chainId,
+  r: 0,
+  s: 0,
 }
-console.log('tx', JSON.stringify(txParams))
+console.log('TX:', JSON.stringify(txParams))
 
-const tx = new Transaction(txParams)
-tx.sign(Buffer.from(envParams.fromPrivKey, 'hex'))
-
-const serialized = `0x${tx.serialize().toString('hex')}`
-console.log('serialized', serialized)
+const encoded = RLP.encode([
+  txParams.nonce,
+  txParams.gasPrice,
+  txParams.gasLimit,
+  txParams.to,
+  txParams.value,
+  txParams.data,
+  txParams.token,
+  txParams.exchanger,
+  txParams.exchangeRate,
+  txParams.v,
+  txParams.r,
+  txParams.s,
+])
+console.log('ENCODED:', encoded.toString('hex'))
